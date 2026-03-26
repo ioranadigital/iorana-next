06d8b4cfc0543c8e45d5176293cb1f7479f71031
ioranadigital <honatuya@gmail.com>
2026-03-26 23:45:25 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,21 +7,21 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 
-// Importación de Páginas desde la nueva carpeta "views"
-import Index from "./../views/Index";
-import NotFound from "./../views/NotFound";
-import Privacidad from "./../views/Privacidad";
-import Terminos from "./../views/Terminos";
-import SeoTecnico from "./../views/servicios/SeoTecnico";
-import PpcPaidSearch from "./../views/servicios/PpcPaidSearch";
-import ContentMarketing from "./../views/servicios/ContentMarketing";
-import Automatizaciones from "./../views/servicios/Automatizaciones";
-import DesarrolloWeb from "./../views/servicios/DesarrolloWeb";
-import ImagenDeMarca from "./../views/servicios/ImagenDeMarca";
-import KitDigital from "./../views/soluciones/KitDigital";
-import DisenoLandings from "./../views/soluciones/DisenoLandings";
-import TodoParaTuNegocio from "./../views/soluciones/TodoParaTuNegocio";
-import ComoEstaOptimizadoMiWeb from "./../views/soluciones/ComoEstaOptimizadoMiWeb";
+// Importación de Páginas (Rutas relativas saliendo de 'app' hacia 'views')
+import Index from "../views/Index";
+import NotFound from "../views/NotFound";
+import Privacidad from "../views/Privacidad";
+import Terminos from "../views/Terminos";
+import SeoTecnico from "../views/servicios/SeoTecnico";
+import PpcPaidSearch from "../views/servicios/PpcPaidSearch";
+import ContentMarketing from "../views/servicios/ContentMarketing";
+import Automatizaciones from "../views/servicios/Automatizaciones";
+import DesarrolloWeb from "../views/servicios/DesarrolloWeb";
+import ImagenDeMarca from "../views/servicios/ImagenDeMarca";
+import KitDigital from "../views/soluciones/KitDigital";
+import DisenoLandings from "../views/soluciones/DisenoLandings";
+import TodoParaTuNegocio from "../views/soluciones/TodoParaTuNegocio";
+import ComoEstaOptimizadoMiWeb from "../views/soluciones/ComoEstaOptimizadoMiWeb";
 
 const queryClient = new QueryClient();
 
@@ -36,16 +36,22 @@ export default function Page() {
             <Route path="/" element={<Index />} />
             <Route path="/privacidad" element={<Privacidad />} />
             <Route path="/terminos" element={<Terminos />} />
+            
+            {/* Rutas de Servicios */}
             <Route path="/servicios/seo-tecnico" element={<SeoTecnico />} />
             <Route path="/servicios/ppc-paid-search" element={<PpcPaidSearch />} />
             <Route path="/servicios/content-marketing" element={<ContentMarketing />} />
             <Route path="/servicios/automatizaciones" element={<Automatizaciones />} />
             <Route path="/servicios/desarrollo-web" element={<DesarrolloWeb />} />
             <Route path="/servicios/imagen-de-marca" element={<ImagenDeMarca />} />
+            
+            {/* Rutas de Soluciones */}
             <Route path="/soluciones/kit-digital" element={<KitDigital />} />
             <Route path="/soluciones/diseno-landings" element={<DisenoLandings />} />
             <Route path="/soluciones/todo-para-tu-negocio" element={<TodoParaTuNegocio />} />
             <Route path="/soluciones/como-esta-optimizado-mi-web" element={<ComoEstaOptimizadoMiWeb />} />
+            
+            {/* 404 */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
