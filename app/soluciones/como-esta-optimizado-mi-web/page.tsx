64a6ed9de3a8434e2f5451a5af569edb11684294
ioranadigital <honatuya@gmail.com>
2026-03-27 16:52:58 +0100

    Update page.tsx

--- app/soluciones/como-esta-optimizado-mi-web/page.tsx
+++ app/soluciones/como-esta-optimizado-mi-web/page.tsx
@@ -1,1 +1,11 @@
+import { Metadata } from "next";
+import ComoEstaOptimizadoMiWeb from "@/views/soluciones/ComoEstaOptimizadoMiWeb";
 
+export const metadata: Metadata = {
+  title: "Auditoría Web Gratuita | Analiza tu Optimización | IORANA Digital",
+  description: "Solicita tu informe gratuito y descubre cómo mejorar el rendimiento y SEO de tu página web.",
+};
+
+export default function Page() {
+  return <ComoEstaOptimizadoMiWeb />;
+}
