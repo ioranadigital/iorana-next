d9563615b4b6bae83869d43fe03f98e077f49022
ioranadigital <honatuya@gmail.com>
2026-03-27 16:52:24 +0100

    Update page.tsx

--- app/soluciones/diseno-de-landings/page.tsx
+++ app/soluciones/diseno-de-landings/page.tsx
@@ -1,1 +1,11 @@
+import { Metadata } from "next";
+import DisenoLandings from "@/views/soluciones/DisenoLandings";
 
+export const metadata: Metadata = {
+  title: "Diseño de Landing Pages de Alta Conversión | IORANA Digital",
+  description: "Creamos páginas de aterrizaje optimizadas para maximizar tus ventas y captar leads cualificados.",
+};
+
+export default function Page() {
+  return <DisenoLandings />;
+}
