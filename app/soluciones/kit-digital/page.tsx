a5072f7d981bad1ce0d27a28db39d4bbd039822d
ioranadigital <honatuya@gmail.com>
2026-03-27 16:53:34 +0100

    Update page.tsx

--- app/soluciones/kit-digital/page.tsx
+++ app/soluciones/kit-digital/page.tsx
@@ -1,1 +1,11 @@
+import { Metadata } from "next";
+import KitDigital from "@/views/soluciones/KitDigital";
 
+export const metadata: Metadata = {
+  title: "Subvenciones Kit Digital | Agente Digitalizador | IORANA Digital",
+  description: "Te ayudamos a gestionar tu bono del Kit Digital para digitalizar tu negocio sin costes.",
+};
+
+export default function Page() {
+  return <KitDigital />;
+}
