c763232d58c155798a6366bdff12eeb63d6ee937
ioranadigital <honatuya@gmail.com>
2026-03-31 16:32:46 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,30 +5,15 @@ const nextConfig = {
       {
         source: '/pyme/vcard',
         destination: '/pymes/todo-para-tu-negocio',
-        permanent: false,
-        statusCode: 301,
-      },
-      {
-        source: '/pymes/vcard',
-        destination: '/pymes/todo-para-tu-negocio',
-        permanent: false,
-        statusCode: 301,
-      },
-       {
-        source: '/vcard',
-        destination: '/pymes/todo-para-tu-negocio',
-        permanent: false,
         statusCode: 301,
       },
       {
         source: '/vcard',
         destination: '/',
-        permanent: false,
         statusCode: 301,
       },
     ]
   },
-  
   // ← aquí va el resto de tu config original si tenías algo
 }
 
